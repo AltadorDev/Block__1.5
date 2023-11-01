45a36c8ba30a037120fd6ac9e9059f2f56f20380
AltadorDev <AltadorDev@gmail.com>
2023-11-01 18:46:04 +0300

    Commit

--- js/script.js
+++ js/script.js
@@ -24,9 +24,9 @@ function mobileSlider() {
           slidesPerView: 3,
         },
       },
-      centeredSlides: true,
-      centerInsufficientSlides: true,
-      centeredSlidesBounds: true,
+        centeredSlides: true,
+        centerInsufficientSlides: true,
+        centeredSlidesBounds: true,
     });
 
     slider.dataset.mobile = "true";
